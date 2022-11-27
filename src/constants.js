4eced94291d2acada1074dbf83e96d43647cd44e
shell300 <shuyu.shang@usask.ca>
2022-11-26 19:54:02 -0800

    My first commit

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'https://around-dot-stayaround.wm.r.appspot.com';
+//export const BASE_URL = 'http://34.83.181.125:8080';
+
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
